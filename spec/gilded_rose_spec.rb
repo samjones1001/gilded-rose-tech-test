b9aa64a8569024b6ea338f19fc7292f67c1ef82a
Sam Jones <sam.jones494@googlemail.com>
2017-03-07 14:08:52 +0000

    added tests for sulfuras

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -124,6 +124,20 @@ describe GildedRose do
       end
     end
 
+    context 'when item is Sulfuras, Hand of Ragnaros' do
+      it 'does not change sell_in' do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 1, 80)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(1)
+      end
+
+      it 'does not change quality' do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 1, 80)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(80)
+      end
+    end
+
 
 
   end
