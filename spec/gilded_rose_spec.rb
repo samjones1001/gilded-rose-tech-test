7c274991ab6d050f41477937b28e563708b4cefc
Sam Jones <sam.jones494@googlemail.com>
2017-03-07 13:59:59 +0000

    Added tests for generic items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,9 +5,73 @@ describe GildedRose do
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    it "lowers the sell_in by one after a day" do
+      items = [Item.new("item", 1, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it "lowers the sell_in by n after n days" do
+      n = 5
+      items = [Item.new("item", n, 0)]
+
+      n.times do
+        GildedRose.new(items).update_quality
+      end
+
+      expect(items[0].sell_in).to eq(0)
+    end
+
+    it "never lowers quality below 0" do
+      items = [Item.new("item", 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(0)
+    end
+
+    context 'generic item' do
+      context 'before sell_in' do
+        it 'lowers quality by one after one day' do
+          items = [Item.new("item", 1, 1)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it 'lowers quality by n after n days' do
+          n = 5
+          items = [Item.new("item", n, n)]
+
+          n.times do
+            GildedRose.new(items).update_quality
+          end
+
+          expect(items[0].quality).to eq(0)
+        end
+      end
+
+      context 'after sell_in' do
+        it 'lowers quality by two after a day' do
+          items = [Item.new("item", 0, 2)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it 'lowers quality by 2n after n days' do
+          n = 5
+          items = [Item.new("item", 0, 10)]
+
+          n.times do
+            GildedRose.new(items).update_quality
+          end
+
+          expect(items[0].quality).to eq(0)
+        end
+      end
+    end
+
+    
+  end
 end
