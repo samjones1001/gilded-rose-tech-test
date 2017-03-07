03b290ce381beeee5855b16b6819f5d5b165bad0
Sam Jones <sam.jones494@googlemail.com>
2017-03-07 14:20:15 +0000

    added tests for backstage pass

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -138,6 +138,97 @@ describe GildedRose do
       end
     end
 
+    context 'when item is Backstage passes to a TAFKAL80ETC concert' do
+      context 'when sell_in is greater than 10' do
+        it 'increases quality by one after each day' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(1)
+        end
+
+        it 'increases quality by n after n days' do
+          n = 5
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 0)]
+
+          n.times do
+            GildedRose.new(items).update_quality
+          end
+
+          expect(items[0].quality).to eq(5)
+        end
+
+        it 'never increases quality beyond 50' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context 'when sell_in is less than or equal to 10 but more than 5' do
+        it 'increases quality by two after each day' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(2)
+        end
+
+        it 'increases quality by 2n after n days' do
+          n = 5
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+
+          n.times do
+            GildedRose.new(items).update_quality
+          end
+
+          expect(items[0].quality).to eq(10)
+        end
+
+        it 'only raises quality to 50 when quality is at 49' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 49)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context 'when sell_in is less than or equal to 5 but more than 0' do
+        it 'increases quality by three after each day' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(3)
+        end
+
+        it 'increases quality by 3n after n days' do
+          n = 5
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+
+          n.times do
+            GildedRose.new(items).update_quality
+          end
+
+          expect(items[0].quality).to eq(15)
+        end
+
+        it 'only raises quality to 50 when quality is at 48 or more' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 48)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context 'after sell_in' do
+        it 'sets quality to 0' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it 'does not change quality' do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+      end
+    end
+
 
 
   end
