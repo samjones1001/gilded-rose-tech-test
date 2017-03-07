a9d145da3c4f81a5a0aba96222d131a11d91585c
Sam Jones <sam.jones494@googlemail.com>
2017-03-07 22:38:57 +0000

    removed if else statements into seperate methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,51 +15,18 @@ class GildedRose
       end
 
       if item.name == "Sulfuras, Hand of Ragnaros"
+        sulfuras_update(item)
         return
       end
 
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      if item.name == "Aged Brie"
+        brie_update(item)
+        return
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        pass_update(item)
+        return
       end
     end
   end
@@ -74,7 +41,52 @@ class GildedRose
     end
     item.sell_in -= 1
   end
-  
+
+  def sulfuras_update(item)
+  end
+
+  def brie_update(item)
+    if item.quality < 50
+      if item.sell_in > 0
+        item.quality += 1
+      else
+        if item.quality < 49
+          item.quality += 2
+        else
+          item.quality += 1
+        end
+      end
+    end
+    item.sell_in -= 1
+  end
+
+  def pass_update(item)
+    if item.quality < 50
+      if item.sell_in > 10
+        item.quality += 1
+      elsif item.sell_in > 5
+        if item.quality < 49
+          item.quality += 2
+        else
+          item.quality += 1
+        end
+      elsif item.sell_in > 0
+        if item .quality < 48
+          item.quality += 3
+        else
+          item.quality = 50
+        end
+      else
+      #  p item.quality -= item.quality
+        item.quality -= item.quality
+      end
+    end
+    if item.sell_in < 1
+      item.quality -= item.quality
+    end
+    item.sell_in -= 1
+  end
+
 end
 
 class Item
