2fcd2880a6ad44c561674557fad758139f74a4c7
Sam Jones <sam.jones494@googlemail.com>
2017-03-08 13:16:03 +0000

    refactor - removed redundant code and changed name of constant to UPDATE_RULES

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,29 +15,28 @@ end
 class ItemUpdater
   attr_reader :item
 
-  SPECIAL_ITEMS = {
-    "Aged Brie" =>                                    Proc.new { |item|
-                                                      item.quality += 1
-                                                      item.quality += 1 if item.sell_in < 1
-                                                      item.quality = 50 if item.quality > 50
-                                                      item.sell_in -= 1
-                                                      },
-    "Sulfuras, Hand of Ragnaros" =>                   Proc.new { |item|
-                                                      },
-    "Backstage passes to a TAFKAL80ETC concert" =>    Proc.new { |item|
+  UPDATE_RULES = {
+    "Aged Brie" =>                                    Proc.new do |item|
+                                                        item.quality += 1
+                                                        item.quality += 1 if item.sell_in < 1
+                                                        item.quality = 50 if item.quality > 50
+                                                        item.sell_in -= 1
+                                                      end,
+    "Sulfuras, Hand of Ragnaros" =>                   Proc.new {},
+    "Backstage passes to a TAFKAL80ETC concert" =>    Proc.new do |item|
                                                         item.quality += 1
                                                         item.quality += 1 if item.sell_in <= 10
                                                         item.quality += 1 if item.sell_in <= 5
                                                         item.quality = 0 if item.sell_in < 1
                                                         item.quality = 50 if item.quality > 50
                                                         item.sell_in -= 1
-                                                      },
-    "Generic item" =>                                 Proc.new { |item|
-                                                      item.quality -= 1
-                                                      item.quality -= 1 if item.sell_in < 1
-                                                      item.quality = 0 if item.quality < 0
-                                                      item.sell_in -= 1
-                                                      }
+                                                      end,
+    "Generic item" =>                                 Proc.new do |item|
+                                                        item.quality -= 1
+                                                        item.quality -= 1 if item.sell_in < 1
+                                                        item.quality = 0 if item.quality < 0
+                                                        item.sell_in -= 1
+                                                      end
   }
 
   def initialize(item)
@@ -45,80 +44,9 @@ class ItemUpdater
   end
 
   def update
-    if SPECIAL_ITEMS.include?(item.name)
-      SPECIAL_ITEMS[item.name].call(item)
-    else
-      SPECIAL_ITEMS["Generic item"].call(item)
-    end
+    UPDATE_RULES.include?(item.name) ? UPDATE_RULES[item.name].call(item) : UPDATE_RULES["Generic item"].call(item)
   end
-
-  private
-
-  def update_picker
-    if item.name == "Aged Brie"
-      SPECIAL_ITEMS[item.name].call(item)
-      return
-    end
-    if SPECIAL_ITEMS.include?(item.name)
-      @updater = eval(SPECIAL_ITEMS[item.name])
-    else
-      @updater = GenericItemUpdater
-    end
-  end
-
-  def increase_quality
-    @item.quality += 1
-  end
-
-  def decrease_quality
-    @item.quality -= 1
-  end
-
-  def update_sell_in
-    @item.sell_in -= 1
-  end
-
-end
-
-
-class BrieUpdater < ItemUpdater
-  def update
-    increase_quality
-    increase_quality if item.sell_in < 1
-
-    item.quality = 50 if item.quality > 50
-    update_sell_in
-  end
-end
-
-
-class SulfurasUpdater < ItemUpdater
-  def update
-  end
-end
-
-
-class BackstagePassUpdater < ItemUpdater
-  def update
-    increase_quality
-    increase_quality if item.sell_in <= 10
-    increase_quality if item.sell_in <= 5
-    item.quality = 0 if item.sell_in < 1
-
-    item.quality = 50 if item.quality > 50
-    update_sell_in
-  end
-end
-
-
-class GenericItemUpdater < ItemUpdater
-  def update
-    decrease_quality
-    decrease_quality if item.sell_in < 1
-
-    item.quality = 0 if item.quality < 0
-    update_sell_in
-  end
+  
 end
 
 
